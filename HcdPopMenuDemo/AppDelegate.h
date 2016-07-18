1713207e6bac6fa64d11d9ced20fbcd1c6fcac9c
Jvaeyhcd <chedahuang@icloud.com>
2016-07-18 16:06:31 +0800

    init project

---
+++ HcdPopMenuDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HcdPopMenuDemo
+//
+//  Created by polesapp-hcd on 16/7/18.
+//  Copyright © 2016年 Polesapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
